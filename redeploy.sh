4286598f5fd6cb32e43be46da887946fc0cbb39a
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2019-07-07 18:03:07 +0200

    Added redeploy script

--- redeploy.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete -f juice-balancer-k8.yml
-docker build -t juice-balancer:latest .
-kubectl apply -f juice-balancer-k8.yml
-sleep 5
-kubectl port-forward service/juice-balancer 3000:3000
