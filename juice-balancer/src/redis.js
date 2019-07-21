2b2c51275f07694fa4746583cda861bf708ff825
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2019-07-21 17:27:09 +0200

    Ensure redis errors are logged out in the same format as the other errors

--- juice-balancer/src/redis.js
+++ juice-balancer/src/redis.js
@@ -1,8 +1,15 @@
 import Redis from 'ioredis';
 import { get } from './config';
+import { logger } from './logger';
 
-export default new Redis({
+const redis = new Redis({
   host: get('redis.host'),
   port: get('redis.port'),
   password: get('redis.password'),
 });
+
+redis.on('error', error => {
+  logger.error(`ioredis: ${error.message}`);
+});
+
+export default redis;
