96c20a14897a3b6790e4b7b6755d10608212f11f
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2019-07-21 17:26:38 +0200

    Add better log format

--- juice-balancer/src/logger.js
+++ juice-balancer/src/logger.js
@@ -1,7 +1,11 @@
 import winston from 'winston';
 
+const myFormat = winston.format.printf(({ level, message, timestamp }) => {
+  return `time="${timestamp}" level="${level}" msg="${message.replace('"', '\\"')}"`;
+});
+
 export const logger = winston.createLogger({
   level: process.env['DEBUG'] ? 'debug' : 'info',
-  format: winston.format.simple(),
+  format: winston.format.combine(winston.format.timestamp(), myFormat),
   transports: [new winston.transports.Console()],
 });
