c4df16dc172153e25e53ed3be13e762e432fc0ca
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2019-08-07 17:50:30 +0200

    Add german translation strings for initial screen

--- juice-balancer/ui/src/translations/de-DE.js
+++ juice-balancer/ui/src/translations/de-DE.js
@@ -1,4 +1,11 @@
 export default {
   getting_started_text:
-    'Wähle einen temnamen so dass wir dich wieder erkennen können. Wenn du dich mit anderen zusammenschließen möchtest, benutzt den gleichen namen.',
+    'Wähle einen Teamnamen so, dass wir dich wieder erkennen können. Wenn du dich mit anderen zusammenschließen möchtest, benutzt den selben Teamnamen.',
+  getting_started: "Los geht's",
+  join_failed_text: 'Das team konnte nicht erstellt / beigetreten werden.',
+  teamname: 'Teamname',
+  teamname_validation_constraints:
+    "Teamname muss aus kleinen Buchstaben, Nummern oder einem '-' bestehen",
+  create_or_join_team_label: 'Team erstellen / beitreten',
+  change_language: 'Sparche ändern',
 };
