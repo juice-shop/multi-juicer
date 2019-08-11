c9645c214190f2e890c07d62da9321f47a6a6625
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2019-08-11 22:39:44 +0200

    Hide logs in testing

--- juice-balancer/src/logger.js
+++ juice-balancer/src/logger.js
@@ -4,8 +4,19 @@ const myFormat = winston.format.printf(({ level, message, timestamp }) => {
   return `time="${timestamp}" level="${level}" msg="${`${message}`.replace('"', '\\"')}"`;
 });
 
+const getLogLevelForEnvironment = env => {
+  switch (env) {
+    case 'development':
+      return 'debug';
+    case 'test':
+      return 'emerg';
+    default:
+      return 'info';
+  }
+};
+
 const logger = winston.createLogger({
-  level: process.env['NODE_ENV'] !== 'production' ? 'debug' : 'info',
+  level: getLogLevelForEnvironment(process.env['NODE_ENV']),
   format: winston.format.combine(winston.format.timestamp(), myFormat),
   transports: [new winston.transports.Console()],
 });
