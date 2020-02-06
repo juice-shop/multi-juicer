1ab827cce2d42c4e7556ccaf69cb344d11d88f61
J12934 <13718901+J12934@users.noreply.github.com>
2020-02-06 13:00:41 +0100

    Remove redis dependency from admin endpoint

--- juice-balancer/src/admin/admin.js
+++ juice-balancer/src/admin/admin.js
@@ -9,7 +9,6 @@ const {
   deleteServiceForTeam,
 } = require('../kubernetes');
 
-const redis = require('../redis');
 const { get } = require('../config');
 const { logger } = require('../logger');
 
@@ -21,7 +20,7 @@ const { logger } = require('../logger');
 function ensureAdminLogin(req, res, next) {
   logger.debug('Running admin check');
   if (req.teamname === `t-${get('admin.username')}`) {
-    logger.debug('Admin check succeded');
+    logger.debug('Admin check succeeded');
     return next();
   }
   return res.status(401).send();
@@ -37,15 +36,6 @@ async function listInstances(req, res) {
     body: { items: instances },
   } = await getJuiceShopInstances();
 
-  const teams = instances.map(instance => instance.metadata.labels.team);
-  const teamTimestampEntryName = teams.map(team => `t-${team}-last-request`);
-
-  const lastConnectTimestamps = teams.length > 0 ? await redis.mget(teamTimestampEntryName) : [];
-  const timeStampsAsMap = lastConnectTimestamps.reduce((map, timeStamp, index) => {
-    map.set(teams[index], timeStamp);
-    return map;
-  }, new Map());
-
   return res.json({
     instances: instances.map(instance => {
       const team = instance.metadata.labels.team;
@@ -54,7 +44,7 @@ async function listInstances(req, res) {
         name: instance.metadata.name,
         ready: instance.status.availableReplicas === 1,
         createdAt: instance.metadata.creationTimestamp.getTime(),
-        lastConnect: parseInt(timeStampsAsMap.get(team), 10),
+        lastConnect: parseInt(instance.metadata.annotations.lastRequest, 10),
       };
     }),
   });
