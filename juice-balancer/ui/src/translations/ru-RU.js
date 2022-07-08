97b9e0490c1d88177bc5c8e04e42efd52514ccd5
Дмитрий Муковкин <d.mukovkin@cft.ru>
2022-07-08 11:12:06 +0700

    Add russian language

---
+++ juice-balancer/ui/src/translations/ru-RU.js
@@ -0,0 +1,41 @@
+const russianTranslations = {
+    getting_started_text:
+        'Тебе предстоит выполнить много нелегких испытаний. Для начала представься. \n              Напиши как тебя зовут, чтобы великий дух сенсея мог тебе выделить место для обучения.\n              Твое имя должно быть на английском в нижнем регистре и может содержать цифры. В начале и в конце не должно быть "-"',
+    getting_started: "Привет, путник!",
+    join_failed_text: 'Ошибка при создании игрового образа',
+    teamname: 'Твоё имя',
+    teamname_validation_constraints:
+        "Твое имя может быть только в нижнем регистре, содержать цифры или '-'",
+    create_or_join_team_label: 'Дальше',
+    change_language: 'Сменить язык',
+    joining_team: 'Войти в комнату {team}',
+    joining_failed:
+        'Неправильный код. Возможно вы его забыли?',
+    team_passcode: 'Код доступа',
+    join_team: 'Дальше',
+    logged_in_as: 'Вошли как',
+    log_out: 'Выйти',
+    reset_passcode: 'Перегенерировать код',
+    team_created: 'Один момент!',
+    passcode_reset: 'Код изменен',
+    passcode_explanation:
+        'Чтобы убедиться, что никто не сможет зайти в твою комнату, мы сгенерировали секретный код. Запиши его в любом месте, чтобы в будущем продолжить обучение.',
+    passcode: 'Код',
+    instance_status_ready: 'Твоя комната готова',
+    instance_status_start_hacking: 'Начать',
+    instance_status_starting: 'Комната готовится. Это может занять некоторое время...',
+    instance_status_starting_taking_longer_than_usual:
+        'Комната все еще готовится. Это может занять некоторое время...',
+    instance_status_timed_out: 'Произошла проблема при создании комнаты',
+    'admin_table.table_header': 'Активные команды',
+    'admin_table.teamname': 'Название команды',
+    'admin_table.ready': 'Статус сервиса',
+    'admin_table.created': 'Было создано',
+    'admin_table.lastUsed': 'Последнее использование',
+    'admin_table.actions': 'Действия',
+    'admin_table.noActiveTeams': 'Никого еще нет',
+    'admin_table.restarting': 'Перезапускаем',
+    'admin_table.restart': 'Рестарт',
+};
+
+export default russianTranslations;
