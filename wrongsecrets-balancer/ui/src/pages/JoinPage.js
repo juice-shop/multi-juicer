ba4d5651c781f01125e5b7ecb52c868d7bb050a3
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2022-09-11 00:23:11 +0200

    removed unused component

--- wrongsecrets-balancer/ui/src/pages/JoinPage.js
+++ wrongsecrets-balancer/ui/src/pages/JoinPage.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { FormattedMessage, defineMessages, injectIntl } from 'react-intl';
 
+import styled from 'styled-components';
+
 import { BodyCard, H2, Label, Input, Form, Button } from '../Components';
 import { InstanceRestartingCard } from '../cards/InstanceRestartingCard';
 import { InstanceNotFoundCard } from '../cards/InstanceNotFoundCard';
