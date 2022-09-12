a4c188f6f4035633bbb50cf30aa5dad80b73e3d1
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2022-09-12 10:08:49 +0200

    Fixed tests

--- wrongsecrets-balancer/src/__mocks__/kubernetes.js
+++ wrongsecrets-balancer/src/__mocks__/kubernetes.js
@@ -1,6 +1,8 @@
 module.exports = {
   createDeploymentForTeam: jest.fn(),
+  createDesktopDeploymentForTeam: jest.fn(),
   createServiceForTeam: jest.fn(),
+  createDesktopServiceForTeam: jest.fn(),
   getJuiceShopInstanceForTeamname: jest.fn(() => ({
     readyReplicas: 1,
     availableReplicas: 1,
