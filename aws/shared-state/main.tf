cd7a4a34b13b958035b34915db7c80c95cc1477c
Jeroen Willemsen <jeroenwillemsen2001@gmail.com>
2022-09-19 09:02:33 +0200

    more instances

---
+++ aws/shared-state/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "~> 1.1"
+  required_providers {
+    aws = {
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "state" {}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
