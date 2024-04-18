08a9c2101798c1c304ed8ede12246875a68d629a
Stuebinger, Bernd <stuebingerb@mediamarktsaturn.com>
2024-04-18 16:02:54 +0200

    Update German translation

--- juice-balancer/ui/src/translations/de-DE.js
+++ juice-balancer/ui/src/translations/de-DE.js
@@ -1,29 +1,29 @@
 const germanTranslations = {
   getting_started_text:
-    'Wähle einen Teamnamen so, dass wir dich wieder erkennen können. Wenn du dich mit anderen zusammenschließen möchtest, benutzt den selben Teamnamen.',
+    'Wähle einen Teamnamen, damit wir dich wiedererkennen können. Wenn du dich mit anderen zusammenschließen möchtest, benutzt denselben Teamnamen.',
   getting_started: "Los geht's",
-  join_failed_text: 'Das team konnte nicht erstellt / beigetreten werden.',
+  join_failed_text: 'Konnte Team nicht erstellen / beitreten.',
   teamname: 'Teamname',
   teamname_validation_constraints:
-    "Teamname muss aus kleinen Buchstaben, Nummern oder einem '-' bestehen",
+    "Teamname muss aus mindestens drei Zeichen bestehen und darf Kleinbuchstaben, Nummern oder '-' enthalten",
   create_or_join_team_label: 'Team erstellen / beitreten',
   change_language: 'Sprache ändern',
   joining_team: 'Team {team} beitreten',
   joining_failed:
-    'Es konnte dem team nicht beigetreten werden. Bist du dir sicher das das Passwort richtig ist?',
-  team_passcode: 'Team Passwort',
-  join_team: 'Team Beitreten',
-  logged_in_as: 'Eingelogged als',
+    'Konnte Team nicht beitreten. Bist du dir sicher, dass das Passwort richtig ist?',
+  team_passcode: 'Team-Passwort',
+  join_team: 'Team beitreten',
+  logged_in_as: 'Angemeldet als',
   log_out: 'Ausloggen',
-  reset_passcode: 'Passwort Zurücksetzen',
-  team_created: 'Team Erstellt',
-  passcode_reset: 'Passwort Zurückgesetzt',
+  reset_passcode: 'Passwort zurücksetzen',
+  team_created: 'Team erstellt',
+  passcode_reset: 'Passwort zurückgesetzt',
   passcode_explanation:
-    'Um sicherzustellen das nicht jeder einfach deinem Team beitreten kann, haben wir ein Passwort erstellt das du mit deinem Teammitgliedern teilen kannst. Deine Teammitglieder brauchen das gleiche Passwort um dem Team beizutreten. Du kannst das Passwort von unten kopieren.',
+    'Um sicherzustellen, dass nicht jeder einfach deinem Team beitreten kann, haben wir ein Passwort erstellt, das du mit deinen Teammitgliedern teilen kannst. Deine Teammitglieder brauchen dasselbe Passwort um dem Team beizutreten. Du kannst das Passwort von unten kopieren.',
   passcode: 'Passwort',
-  instance_status_ready: 'Juice Shop Instanz Bereit',
-  instance_status_start_hacking: 'Anfangen zu Hacken',
-  instance_status_starting: 'Juice Shop Instanz startet',
+  instance_status_ready: 'Juice Shop-Instanz bereit',
+  instance_status_start_hacking: 'Anfangen zu hacken',
+  instance_status_starting: 'Juice Shop-Instanz startet',
   instance_status_starting_taking_longer_than_usual:
     'Das Starten der Instanz dauert länger als normal...',
   instance_status_timed_out: 'Das Starten der Instanz hat zu lange gedauert',
@@ -31,11 +31,11 @@ const germanTranslations = {
   'admin_table.teamname': 'Teamname',
   'admin_table.ready': 'Bereit',
   'admin_table.created': 'Erstellt',
-  'admin_table.lastUsed': 'Zuletzt Genutzt',
+  'admin_table.lastUsed': 'Zuletzt genutzt',
   'admin_table.actions': 'Aktionen',
   'admin_table.noActiveTeams': 'Keine aktiven Teams',
   'admin_table.restarting': 'Wird neu gestartet',
-  'admin_table.restart': 'Neustarten',
+  'admin_table.restart': 'Neu starten',
 };
 
 export default germanTranslations;
