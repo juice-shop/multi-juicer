b2d530ae26d41600f93ee3653aff948949d9d589
Stuebinger, Bernd <stuebingerb@mediamarktsaturn.com>
2024-04-18 16:08:06 +0200

    Fix typos

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "This Script canbe used to 'easily' build all MultiJuicer Components and install them to a local kubernetes cluster"
+echo "This Script can be used to 'easily' build all MultiJuicer components and install them to a local kubernetes cluster"
 echo "For this to work the local kubernetes cluster must have access to the same local registry / image cache which 'docker build ...' writes its image to"
 echo "For example docker-desktop with its included k8s cluster"
 
