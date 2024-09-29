5611cb49a9d228a516ff877c4250b0b5de08c886
Jannik Hollenbach <Jannik Hollenbach>
2024-09-29 04:22:18 +0200

    Remove create-react-app and move to vite

---
+++ juice-balancer/ui/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: 'build',
+    },
+    plugins: [react()],
+    server: {
+        proxy: {
+            // proxy every request but those that start with /balancer to port 3000
+            '^/balancer/.*': "http://localhost:4000",
+        },
+    }
+  };
+});
