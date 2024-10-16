d86f850a40539417743b9c39e0c8b274283240d8
Jannik Hollenbach <jannik@hollenbach.de>
2024-10-16 20:00:06 +0200

    Try to replicate node.js hmac signed cookie

--- balancer/main.go
+++ balancer/main.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+
+	signutil "github.com/juice-shop/multi-juicer/balancer/pkg/signutil"
 )
 
 // newReverseProxy creates a reverse proxy for a given target URL.
@@ -18,22 +21,36 @@ func newReverseProxy(target string) *httputil.ReverseProxy {
 	return httputil.NewSingleHostReverseProxy(url)
 }
 
-// proxyHandler determines the target based on the "X-Team" header and proxies the request.
+// proxyHandler determines the target based on the "balancer" header and proxies the request.
 func proxyHandler(w http.ResponseWriter, r *http.Request) {
-	team, err := r.Cookie("x-team")
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Failed to read X-Team cookie: %v", err), http.StatusBadRequest)
+	cookieSecret := os.Getenv("COOKIEPARSER_SECRET")
+	if cookieSecret == "" {
+		http.Error(w, "", http.StatusInternalServerError)
+		log.Default().Fatalf("COOKIEPARSER_SECRET environment variable must be set")
+		return
 	}
+
+	teamSigned, err := r.Cookie("balancer")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to read balancer cookie: %v", err), http.StatusBadRequest)
+		return
+	}
+	team, err := signutil.Unsign(teamSigned.Value, cookieSecret)
+	if err != nil {
+		http.Error(w, "Team cookie invalid!", http.StatusUnauthorized)
+		return
+	}
+
 	var target string
 
-	if team.Value == "" {
-		http.Error(w, fmt.Sprintf("Unknown X-Team cookie value: '%s'", team.Value), http.StatusBadRequest)
+	if team == "" {
+		http.Error(w, fmt.Sprintf("Empty team for balancer cookie: '%s',", teamSigned), http.StatusBadRequest)
 		return
 	} else {
-		target = fmt.Sprintf("http://t-%s-juiceshop.default.svc.cluster.local:3000", team.Value)
+		target = fmt.Sprintf("http://t-%s-juiceshop.default.svc.cluster.local:3000", team)
 	}
 
-	log.Default().Printf("proxing request: %s %s to %s", r.Method, r.URL, target)
+	log.Default().Printf("proxing request for team (%s): %s %s to %s", team, r.Method, r.URL, target)
 
 	proxy := newReverseProxy(target)
 	// Rewrite the request to the target server
---
+++ balancer/pkg/signutil/signutil.go
@@ -0,0 +1,58 @@
+package signutil
+
+// based on https://github.com/tj/node-cookie-signature
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
+	"strings"
+)
+
+// Sign the given `val` with `secret`.
+// Returns the signed string.
+func Sign(val, secret string) (string, error) {
+	if val == "" {
+		return "", errors.New("cookie value must be provided as a string")
+	}
+	if secret == "" {
+		return "", errors.New("secret key must be provided")
+	}
+
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write([]byte(val))
+	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
+
+	// Remove any trailing '=' characters from the base64-encoded string
+	signature = strings.TrimRight(signature, "=")
+	return val + "." + signature, nil
+}
+
+// Unsign and decode the given `input` with `secret`.
+// Returns the original value if the signature is valid, otherwise returns false.
+func Unsign(input, secret string) (string, error) {
+	if input == "" {
+		return "", errors.New("signed cookie string must be provided")
+	}
+	if secret == "" {
+		return "", errors.New("secret key must be provided")
+	}
+
+	lastDotIndex := strings.LastIndex(input, ".")
+	if lastDotIndex == -1 {
+		return "", nil
+	}
+
+	tentativeValue := input[:lastDotIndex]
+	expectedSignedValue, err := Sign(tentativeValue, secret)
+	if err != nil {
+		return "", err
+	}
+
+	// Use hmac.Equal for timing-safe comparison
+	if hmac.Equal([]byte(input), []byte(expectedSignedValue)) {
+		return tentativeValue, nil
+	}
+	return "", errors.New("signature mismatch")
+}
