3d28dc43a70430b45677fd931ab0ac00844f3ac0
Jannik Hollenbach <jannik.hollenbach@iteratec.com>
2024-10-18 14:57:19 +0200

    Make static file test match other test file

--- balancer/routes/staticFiles_test.go
+++ balancer/routes/staticFiles_test.go
@@ -11,17 +11,19 @@ import (
 
 // Test for the HelloHandler
 func TestStaticFileHandler(t *testing.T) {
-	req, _ := http.NewRequest("GET", "/balancer/", nil)
-	rr := httptest.NewRecorder()
+	t.Run("should return static files when requested", func(t *testing.T) {
+		req, _ := http.NewRequest("GET", "/balancer/", nil)
+		rr := httptest.NewRecorder()
 
-	server := http.NewServeMux()
-	bundle := testutil.NewTestBundle()
-	bundle.StaticAssetsDirectory = "../ui/build/"
-	AddRoutes(server, bundle)
+		server := http.NewServeMux()
+		bundle := testutil.NewTestBundle()
+		bundle.StaticAssetsDirectory = "../ui/build/"
+		AddRoutes(server, bundle)
 
-	server.ServeHTTP(rr, req)
+		server.ServeHTTP(rr, req)
 
-	assert.Equal(t, rr.Code, http.StatusOK)
-	assert.Equal(t, rr.Header().Get("Content-Type"), "text/html; charset=utf-8")
-	assert.Contains(t, rr.Body.String(), "<title>MultiJuicer</title>")
+		assert.Equal(t, rr.Code, http.StatusOK)
+		assert.Equal(t, rr.Header().Get("Content-Type"), "text/html; charset=utf-8")
+		assert.Contains(t, rr.Body.String(), "<title>MultiJuicer</title>")
+	})
 }
