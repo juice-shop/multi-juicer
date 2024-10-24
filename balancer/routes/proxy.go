d2d8bc0fd49710f590e43ae04ad7c22bca3784f7
Jannik Hollenbach <jannik.hollenbach@iteratec.com>
2024-10-24 15:58:12 +0200

    Add instance up checks
    todo: this isn't safe map writes need to be synced / locked

--- balancer/routes/proxy.go
+++ balancer/routes/proxy.go
@@ -1,14 +1,25 @@
 package routes
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"time"
 
 	"github.com/juice-shop/multi-juicer/balancer/pkg/bundle"
 	"github.com/juice-shop/multi-juicer/balancer/pkg/signutil"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+var instanceUpCache = map[string]int64{}
+
+func clearInstanceUpCache() {
+	instanceUpCache = map[string]int64{}
+}
 
 // newReverseProxy creates a reverse proxy for a given target URL.
 func newReverseProxy(target string) *httputil.ReverseProxy {
@@ -43,9 +54,38 @@ func handleProxy(bundle *bundle.Bundle) http.Handler {
 				return
 			}
 
+			if !wasInstanceUptimeStatusCheckedRecently(team) {
+				if isInstanceUp(bundle, team) {
+					instanceUpCache[team] = time.Now().UnixMilli()
+				} else {
+					bundle.Log.Printf("Instance for team (%s) is down. Redirecting to balancer page.", team)
+					http.Redirect(responseWriter, req, fmt.Sprintf("/balancer/?msg=instance-restarting&teamname=%s", team), http.StatusFound)
+					return
+				}
+			}
+
 			target := bundle.GetJuiceShopUrlForTeam(team, bundle)
 			bundle.Log.Printf("Proxying request for team (%s): %s %s to %s", team, req.Method, req.URL, target)
 			// Rewrite the request to the target server
 			newReverseProxy(target).ServeHTTP(responseWriter, req)
 		})
+}
+
+// checks if the instance uptime status was checked in the last ten seconds by looking into the instanceUpCache
+func wasInstanceUptimeStatusCheckedRecently(team string) bool {
+	lastConnect, ok := instanceUpCache[team]
+	return ok && lastConnect > time.Now().Add(-10*time.Second).UnixMilli()
+}
+
+func isInstanceUp(bundle *bundle.Bundle, team string) bool {
+	deployment, err := bundle.ClientSet.AppsV1().Deployments(bundle.RuntimeEnvironment.Namespace).Get(context.Background(), fmt.Sprintf("juiceshop-%s", team), metav1.GetOptions{})
+
+	if errors.IsNotFound(err) {
+		return false
+	} else if err != nil {
+		bundle.Log.Printf("Failed to lookup if a instance is up in the kubernetes api. Assuming it's missing: %s", err)
+		return false
+	} else {
+		return deployment.Status.ReadyReplicas > 0
+	}
 }
