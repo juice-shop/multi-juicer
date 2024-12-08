b46a22d9123f5db62aa97db755c0eb64a3808565
Jannik Hollenbach <jannik@hollenbach.de>
2024-12-08 21:07:37 +0100

    Enable strict mode

--- balancer/ui/src/index.tsx
+++ balancer/ui/src/index.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { StrictMode } from "react";
 
 const root = createRoot(document.getElementById("root")!);
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
