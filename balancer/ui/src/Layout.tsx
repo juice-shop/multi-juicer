ca42b0c5f4c94daf38a22db3070b0246600a789f
Jannik Hollenbach <jannik.hollenbach@owasp.org>
2025-10-29 18:50:02 +0100

    Show logout button for admin teams too

--- balancer/ui/src/Layout.tsx
+++ balancer/ui/src/Layout.tsx
@@ -170,7 +170,7 @@ function LogoutMenuItem({
     }
   }
 
-  if (!activeTeam || activeTeam === "admin") {
+  if (!activeTeam) {
     return null;
   }
 
@@ -224,7 +224,7 @@ function ContextMenuContent({
           />
         ))}
       </div>
-      {activeTeam && activeTeam !== "admin" && (
+      {activeTeam && (
         <>
           <div className="border-t border-gray-200 dark:border-gray-700 my-1" />
           <div className="py-1">
