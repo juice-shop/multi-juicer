38781d407832db779b130ddce7040d231ed6a719
Jannik Hollenbach <jannik.hollenbach@owasp.org>
2026-05-09 21:32:57 +0200

    Update to the now recommended way to include the react compiler via vite

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,8 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-import react from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -13,13 +14,7 @@ export default defineConfig(() => {
       outDir: "build",
       sourcemap: true, // we are an open source project, we have nothing to hide :D
     },
-    plugins: [
-      react({
-        babel: {
-          plugins: [["babel-plugin-react-compiler", {}]],
-        },
-      }),
-    ],
+    plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
